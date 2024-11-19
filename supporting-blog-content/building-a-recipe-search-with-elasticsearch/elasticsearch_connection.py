a8ead1531cc6ba6d51a18143aac3c3b623cf0229
Xiaoguo Liu <liu_xiao_guo@yahoo.com>
2024-11-19 15:55:49 +0800

    Updated the files under supporting-blog-content/building-a-recipe-search-with-elasticsearch

--- supporting-blog-content/building-a-recipe-search-with-elasticsearch/elasticsearch_connection.py
+++ supporting-blog-content/building-a-recipe-search-with-elasticsearch/elasticsearch_connection.py
@@ -1,25 +1,44 @@
-import yaml
+# import yaml
+import os
+from dotenv import load_dotenv
 from elasticsearch import Elasticsearch, AsyncElasticsearch
 
+load_dotenv()
+
+elastic_user = os.getenv('ES_USER')
 
 class ElasticsearchConnection:
+    elastic_user = os.getenv('ES_USER')
+    elastic_password = os.getenv('ES_PASSWORD')
+    elastic_endpoint = os.getenv("ES_ENDPOINT")
+    url = f"https://{elastic_user}:{elastic_password}@{elastic_endpoint}:9200"
 
-    def __init__(self, config_file="config.yml"):
-        with open(config_file, "r") as f:
-            config = yaml.safe_load(f)
-            self.client = Elasticsearch(
-                cloud_id=config["cloud_id"], api_key=config["api_key"]
-            )
+    # def __init__(self, config_file="config.yml"):
+    #     with open(config_file, "r") as f:
+    #         config = yaml.safe_load(f)
+    #         self.client = Elasticsearch(
+    #             cloud_id=config["cloud_id"], api_key=config["api_key"]
+    #         )
+            
+    def __init__(self):
+        self.client = Elasticsearch( self.__class__.url, 
+                                     ca_certs = "./http_ca.crt", 
+                                     verify_certs = True)
+        print(self.client.info())
 
     def get_client(self):
         return self.client
 
     def get_async_client(self):
-        with open("config.yml", "r") as f:
-            config = yaml.safe_load(f)
-            self.client = AsyncElasticsearch(
-                cloud_id=config["cloud_id"],
-                api_key=config["api_key"],
-                request_timeout=240,
-            )
+        # with open("config.yml", "r") as f:
+        #     config = yaml.safe_load(f)
+            # self.client = AsyncElasticsearch(
+            #     cloud_id=config["cloud_id"],
+            #     api_key=config["api_key"],
+            #     request_timeout=240,
+            # )
+        self.client = AsyncElasticsearch( self.__class__.url, 
+                                          ca_certs = "./http_ca.crt", 
+                                          verify_certs = True,
+                                          request_timeout=240)            
         return self.client
--- supporting-blog-content/building-a-recipe-search-with-elasticsearch/infra.py
+++ supporting-blog-content/building-a-recipe-search-with-elasticsearch/infra.py
@@ -4,38 +4,44 @@ client = ElasticsearchConnection().get_client()
 
 
 def create_index_embedding():
-    response = client.indices.create(
-        index="grocery-catalog-elser",
-        mappings={
-            "properties": {
-                "id": {"type": "integer"},
-                "name": {"type": "text"},
-                "description": {"type": "text", "copy_to": "description_embedding"},
-                "category": {"type": "keyword"},
-                "brand": {"type": "keyword"},
-                "price": {"type": "float"},
-                "unit": {"type": "keyword"},
-                "description_embedding": {
-                    "type": "semantic_text",
-                    "inference_id": "elser_embeddings",
-                },
-            }
-        },
-    )
-    print(response)
-
+    try:
+        response = client.indices.create(
+            index="grocery-catalog-elser",
+            mappings={
+                "properties": {
+                    "id": {"type": "integer"},
+                    "name": {"type": "text"},
+                    "description": {"type": "text", "copy_to": "description_embedding"},
+                    "category": {"type": "keyword"},
+                    "brand": {"type": "keyword"},
+                    "price": {"type": "float"},
+                    "unit": {"type": "keyword"},
+                    "description_embedding": {
+                        "type": "semantic_text",
+                        "inference_id": "elser_embeddings",
+                    },
+                }
+            },
+        )
+        print(response)
+    except:
+        pass
 
 def create_inference():
-    response = client.inference.put(
-        inference_id="elser_embeddings",
-        task_type="sparse_embedding",
-        body={
-            "service": "elser",
-            "service_settings": {"num_allocations": 1, "num_threads": 1},
-        },
-    )
-    print(response)
-
+    models = client.ml.get_trained_models()
+    # print(models)
+    try:
+        response = client.inference.put(
+            inference_id = "elser_embeddings",
+            task_type="sparse_embedding",
+            body={
+                "service": "elser",
+                "service_settings": {"num_allocations": 1, "num_threads": 1},
+            },
+        )
+        print(response)
+    except:
+        pass
 
 if __name__ == "__main__":
 
